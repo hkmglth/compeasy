437175dbca8a5b1c784dbf5d1302ade779f1b387
Taha <59073087+hkmglth@users.noreply.github.com>
2023-01-11 19:05:46 +0300

    created dashboard module

--- src/components/Company/CompanySimple.tsx
+++ src/components/Company/CompanySimple.tsx
@@ -1,7 +1,49 @@
+import { Avatar, Button } from "antd";
+import { ICompanyDto } from "dtos/Companies";
 import React from "react";
+import {
+  UserOutlined,
+  ShopOutlined,
+  PhoneOutlined,
+  NumberOutlined,
+  DatabaseOutlined,
+} from "@ant-design/icons";
+type ICompanySimpleProps = {
+  company: ICompanyDto;
+};
 
-const CompanySimple = () => {
-  return <div>CompanySimple</div>;
+const CompanySimple = ({ company }: ICompanySimpleProps) => {
+  return (
+    <div className="bg-gray-200 hover:scale-105 transition-all duration-150 ease-in-out rounded-md overflow-hidden shadow-lg flex flex-col items-center justify-center w-full md:w-[calc(50%-1rem)] lg:w-[calc(25%-1rem)]">
+      <div className="bg-sky-400 shadow-lg relative h-24 w-full px-3">
+        <p className="text-white flex gap-x-3 p-3 font-medium">
+          <ShopOutlined />
+          {company.fields}
+        </p>
+        <Avatar
+          className="absolute bg-sky-200 -bottom-6 left-1/2 -translate-x-1/2 shadow-md"
+          src={company.companyPic}
+          size={80}
+          icon={<UserOutlined />}
+        />
+      </div>
+
+      <div className="mt-5 py-3 flex flex-col gap-1">
+        <p className="!m-0 !p-0 flex flex-row gap-x-3 items-center justify-center text-center font-medium text-gray-700 text-base">
+          <DatabaseOutlined />
+          {company.companyName}
+        </p>
+        <p className="!m-0 !p-0 flex flex-row gap-x-3 items-center justify-center text-center text-gray-700 text-base">
+          <PhoneOutlined />
+          {company.companyPhone}
+        </p>
+        <p className="!m-0 !p-0 flex flex-row gap-x-3 items-center justify-center text-center text-gray-700 text-base">
+          <NumberOutlined />
+          {company.vatNumber}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default CompanySimple;
--- src/modules/DashboardLanding/DashboardLanding.tsx
+++ src/modules/DashboardLanding/DashboardLanding.tsx
@@ -1,7 +1,89 @@
-import React from "react";
+import { getAllFieldStats, getLastActionCompanies } from "api/CompaniesApi";
+import CompanySimple from "components/Company/CompanySimple";
+import { ICompaniesDto } from "dtos/Companies";
+import { useEffect, useRef, useState } from "react";
+import autoAnimate from "@formkit/auto-animate";
+import { Divider } from "antd";
+import { Pie } from "@ant-design/plots";
+import { IMultipleFieldStats } from "dtos/Fields";
+
+import { ClockCircleOutlined, PieChartOutlined } from "@ant-design/icons";
 
 const DashboardLanding = () => {
-  return <div>DashboardLanding</div>;
+  const parentLastActions = useRef<HTMLDivElement>(null);
+
+  const [lastActionCompanies, setLastActionCompanies] = useState<ICompaniesDto>(
+    []
+  );
+
+  const [fieldStats, setFieldStats] = useState<IMultipleFieldStats>([]);
+
+  const getLastActions = async () => {
+    const response = await getLastActionCompanies();
+    setLastActionCompanies(response);
+  };
+
+  const getFieldStats = async () => {
+    const response = await getAllFieldStats();
+    setFieldStats(response);
+  };
+
+  useEffect(() => {
+    getLastActions();
+    getFieldStats();
+  }, []);
+
+  useEffect(() => {
+    parentLastActions.current && autoAnimate(parentLastActions.current);
+  }, [parentLastActions]);
+
+  const config = {
+    appendPadding: 10,
+    data: fieldStats,
+    angleField: "value",
+    colorField: "type",
+    radius: 0.75,
+    label: {
+      type: "spider",
+      labelHeight: 28,
+      content: "{name}\n{percentage}",
+    },
+    interactions: [
+      {
+        type: "element-selected",
+      },
+      {
+        type: "element-active",
+      },
+    ],
+  };
+
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div className="flex flex-col gap-y-3">
+        <Divider orientation="left">
+          <p className="font-bold flex flex-row gap-x-3 text-gray-500 text-lg m-0 p-0">
+            <ClockCircleOutlined />
+            Recent
+          </p>
+        </Divider>
+        <div className="flex flex-wrap gap-4 flex-row" ref={parentLastActions}>
+          {lastActionCompanies.map((company) => (
+            <CompanySimple key={company.id} company={company} />
+          ))}
+        </div>
+      </div>
+      <div>
+        <Divider orientation="left">
+          <p className="font-bold flex flex-row gap-x-3 text-gray-500 text-lg m-0 p-0">
+            <PieChartOutlined />
+            Field Stats
+          </p>
+        </Divider>
+        <Pie {...config} />
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLanding;
